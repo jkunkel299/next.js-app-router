304ba42e228799be29a2d512d883b5ea997172f9
jkunkel299 <jmk299pitt@gmail.com>
2025-05-15 13:55:02 -0400

    nextjs-dashboard error handling

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
+import { error } from 'console';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -27,10 +28,15 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try {
+        await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+        `;
+    } catch (error) {
+        console.error(error);
+    }
+    
 
     /* Once the database has been updated, the /dashboard/invoices path will 
     be revalidated, and fresh data will be fetched from the server */
@@ -47,17 +53,27 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql `
+    try {
+        await sql `
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
-    `;
+        `;
+    } catch(error) {
+        console.error(error)
+    }
+    
 
     revalidatePath('dashboard/invoices'); 
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql `DELETE FROM invoices WHERE id = ${id}`;
+    try{
+        await sql `DELETE FROM invoices WHERE id = ${id}`;
+    } catch(error) {
+        console.error(error)
+    }
+    
     revalidatePath('dashboard/invoices'); 
 }
